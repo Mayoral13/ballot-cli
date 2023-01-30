c5c6295e549068f892ebb389fa9945d0289e4048
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-01-30 03:17:32 +0100

    Something went wrong

--- main.go
+++ main.go
@@ -7,38 +7,35 @@ import (
 	"strings"
 	"strconv"
 )
-type Candidate struct{
+type Candidate struct{ //TO STORE CANDIDATES DETAILS
 	name string;
 	id int;
 	votes int;
 	winner bool;
 }
-var started bool;
-var ended bool;
-var counter int;
-var candidates[] Candidate;
-var users = make(map[int] Candidate);
+var started bool; //TO CHECK IF VOTING HAS STARTED
+var ended bool; //TO CHECK IF VOTING HAS ENDED
+var counter int; //COUNTER TO KEEP COUNT OF CANDIDATE'S ID
+var candidates[] Candidate; //SLICE TO STORE ALL CANDIDATES
+var users = make(map[int] Candidate); //MAPPING OF CANDIDATE'S ID TO THE CANDIDATE STRUCT
 
-func main(){
+func main(){ // MAIN PROGRAM CODE SEEMS SELF EXPLANATORY
 	for true{
 	fmt.Println("Welcome to the ballot program")
 	fmt.Println("Enter 1 to enter ballot")
-	fmt.Println("Enter 2 to view result")
+	fmt.Println("Enter 2 to end voting")
 	fmt.Println("Enter 3 to start/end election")
-	fmt.Println("Enter 4 to poll votes")
+	fmt.Println("Enter 4 to vote") //DURING VOTING TO POLL RESULT INPUT AN ID THAT DOES NOT EXIST
 	input := bufio.NewReader(os.Stdin)
     text,_ := input.ReadString('\n')
     value := strings.TrimSpace(text)
 	if !(value == "1" || value == "2" || value == "3" || value =="4"){
      fmt.Println("Invalid input")
-	}else if(value == "1"){
+	}else if(value == "2"){
+		EndVote()
+		}else if(value == "1"){
 		AddUser();
 		fmt.Println("User added")
-	}else if(value == "2" && started){
-		fmt.Println("The result are")
-		ViewResult()
-	}else if(value == "2" && !started){
-		fmt.Println("Election has not started")
 	}else if(value == "3"){
 		if(started){
         started = false;
@@ -62,70 +59,54 @@ func main(){
 }
 }
 
-func AddUser(){
-if(!started){
+func AddUser(){ //TO REGISTER CANDIDATES FOR ELECTION
+if(!started){ // CANNOT EXECUTE UNLESS STARTED IS TRUE
 fmt.Println("Enter name")
 input := bufio.NewReader(os.Stdin)
 text,_ := input.ReadString('\n')
 value := strings.TrimSpace(text)
 if(value != ""){
 counter++;
-candidates = append(candidates,Candidate{name:value,id:counter,votes:0,winner:false});
-users[counter] = Candidate{name:value,id:counter,votes:0,winner:false}
+candidates = append(candidates,Candidate{name:value,id:counter,votes:0,winner:false});// STORES CANDIDATES DETAILS
+users[counter] = Candidate{name:value,id:counter,votes:0,winner:false} 
 }else{
 	fmt.Println("ERROR INVALID INPUT")
 }
 }
 }
-func VoteUser(){
+func VoteUser(){ // TO VOTE FOR SPECIFIC USER BASED ON USER INPUT
 	if(started){
-		for i:=0; i<len(candidates); i++{
+		for i:=0; i<len(candidates); i++{//DISPLAYS ALL CANDIDATES NAMES AND THEIR ID 
 			fmt.Println("Candidate names are : ",candidates[i].name)
 			fmt.Println("Candidate id are : ",candidates[i].id)
 		}
 	}
-	for true{
 	fmt.Println("Enter Candidate's ID to register vote")
 	input := bufio.NewReader(os.Stdin)
     text,_ := input.ReadString('\n')
     value := strings.TrimSpace(text)
 	id,_ := strconv.Atoi(value)
-	if !(UserExist(id)){
-		fmt.Println("Unregistered candidate")
-		EndVote();
+	if !(UserExist(id)){ // A FUNCTION CHECKS TO SEE IF A USER EXISTS BY PASSING ITS ID AS A PARAMETER  
+		fmt.Println("NOT FOUND")
 	}else if(UserExist(id)){
-	if votes,ok := users[id];ok{
-		fmt.Println("You voted for candidate ", users[id].id)
-		votes.votes += 1;
+	if votes,ok := users[id];ok{ // WORKAROUND TO REINITIALIZE CANDIDATE STRUCT DATA
+		votes.votes++;
 		users[id] = votes;
+		fmt.Println("You voted for candidate ", users[id].id)
 	}else{
 		fmt.Println("NO VOTE")
 	}
 	}
-	}
-	
 }
-func ViewResult(){
-	if(started){
-		for i:=0; i<len(candidates); i++{
-			fmt.Println("Candidate name is : ",candidates[i].name)
-			fmt.Println("Candidate id is : ",candidates[i].id)
-			fmt.Println("Candidate votes are : ",candidates[i].votes)
-			fmt.Println("Candidate win ? : ",candidates[i].winner)
-		}
-	}else{
-		fmt.Println("ELECTION NOT STARTED")
-	}
-}
-func UserExist(id int) (bool){
+
+func UserExist(id int) (bool){ //FUNCTION TO CHECK IF A USER EXISTS
 if(users[id].id != 0){
  return true;
 }else{
-	fmt.Println("User not found")
 	return false
 }
 }
-func EndVote(){
+func EndVote(){ // FUNCTION TO POLL ALL VOTES AND DECLARE WINNER
 	var winner int;
 	if(started){
 		fmt.Println("Voting ended")
@@ -139,6 +120,7 @@ func EndVote(){
 			}
 		}
 	}else{
+		main()
 		fmt.Println("VOTING NOT STARTED")
 	}
 	ended = true;
---
+++ readme.md
@@ -0,0 +1,2 @@
+This is a simple ballot program written in go
+There are several functions that perform specific tasks
